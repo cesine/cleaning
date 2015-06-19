4074a2e7fb7b967704497e988fc57eedbdd400ee
cesine <cesine@yahoo.com>
2015-06-19 10:55:21 -0400

    the library takes in time lengths, not timestamps

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,8 +1,8 @@
 var TimeFormat = require('../index');
 
 describe('load', function() {
-	var sampleTimestamp = 1434723131965;
-	var sampleTime = 1249;
+	var sampleTimestamp = 31966;
+	var sampleTime = 124009;
 	it('should load', function() {
 		expect(TimeFormat).toBeDefined();
 		expect(TimeFormat.sec_to_hhmmss_improved).toBeDefined();
@@ -10,43 +10,43 @@ describe('load', function() {
 	});
 
 	it('should convert seconds to hhmmss format', function() {
-		expect(TimeFormat.seconds_to_hhmmss(sampleTime)).toEqual('56681:58:21');
+		expect(TimeFormat.seconds_to_hhmmss(sampleTime)).toEqual('34:26:49');
 	});
 
 	it('should convert seconds to hhmm format', function() {
-		expect(TimeFormat.seconds_to_hhmm(sampleTime)).toEqual('398534203:19 h');
+		expect(TimeFormat.seconds_to_hhmm(sampleTime)).toEqual('34:26 h');
 	});
 
 	it('should load sec_to_hhmmss_improved', function() {
-		expect(TimeFormat.sec_to_hhmmss_improved(sampleTime)).toEqual("<span class='duration'><strong>398534203:19</strong>:25</span>");
+		expect(TimeFormat.sec_to_hhmmss_improved(sampleTime)).toEqual("<span class='duration'><strong>34:26</strong>:49</span>");
 	});
 
 	it('should load sec_to_decimal_hours', function() {
-		expect(TimeFormat.sec_to_decimal_hours(sampleTime)).toEqual('398534203.32 h');
+		expect(TimeFormat.sec_to_decimal_hours(sampleTime)).toEqual('34.45 h');
 	});
 
 	it('should load seconds_to_ext_hhmmss', function() {
-		expect(TimeFormat.seconds_to_ext_hhmmss(sampleTime)).toEqual('56681:58:21');
+		expect(TimeFormat.seconds_to_ext_hhmmss(sampleTime)).toEqual('34:26:49');
 	});
 
 	it('should load seconds_to_hhmmss', function() {
-		expect(TimeFormat.seconds_to_hhmmss(sampleTime)).toEqual('56681:58:21');
+		expect(TimeFormat.seconds_to_hhmmss(sampleTime)).toEqual('34:26:49');
 	});
 
 	it('should load seconds_to_hhmm', function() {
-		expect(TimeFormat.seconds_to_hhmm(sampleTime)).toEqual('398534203:19 h');
+		expect(TimeFormat.seconds_to_hhmm(sampleTime)).toEqual('34:26 h');
 	});
 
 	it('should load seconds_to_small_hhmm', function() {
-		expect(TimeFormat.seconds_to_small_hhmm(sampleTime)).toEqual('398534203:19');
+		expect(TimeFormat.seconds_to_small_hhmm(sampleTime)).toEqual('34:26');
 	});
 
 	it('should load seconds_to_pretty_hhmm', function() {
-		expect(TimeFormat.seconds_to_pretty_hhmm(sampleTime)).toEqual('398534203 h 19 min');
+		expect(TimeFormat.seconds_to_pretty_hhmm(sampleTime)).toEqual('34 h 26 min');
 	});
 
 	it('should load milliseconds_to_hhmmss', function() {
-		expect(TimeFormat.milliseconds_to_hhmmss(sampleTime)).toEqual('398534:12:11');
+		expect(TimeFormat.milliseconds_to_hhmmss(sampleTime)).toEqual('34:26:49');
 	});
 
 });
